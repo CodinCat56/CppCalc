a95a612bf39683e1964aa16c24862ce0a500b125
Lucas Rausch <89104643+CodinCat56@users.noreply.github.com>
2021-08-21 09:16:49 -0500

    The code!

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*
+	this program will compile and print 2 numbers from the user and will be added.
+
+	*/
+	int a;
+	cout << "A number, please! ";
+	cin >> a;
+	int b;
+	cout << "Another one, Please! ~(uwu)~ ";
+	cin >> b;
+	cout << "Here it is! ";
+	cout << a + b;
+
+
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
